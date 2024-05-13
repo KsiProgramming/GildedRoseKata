f07a275c13a9c78478c8eed21331ad343206dee8
Ksi <labani.said@outlook.com>
2024-05-13 23:54:12 +0200

    Add item class

---
+++ src/Core/OriginalCode/Item.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Item.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.OriginalCode
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+}
