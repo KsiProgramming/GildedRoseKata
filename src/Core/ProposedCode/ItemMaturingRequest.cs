ffc22981b34c3b0a084dff055ab62bde1fffd78c
Ksi <labani.said@outlook.com>
2024-05-15 00:00:54 +0200

    Add ItemMaturingRequest.cs

---
+++ src/Core/ProposedCode/ItemMaturingRequest.cs
@@ -0,0 +1,10 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ItemMaturingRequest.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.ProposedCode
+{
+    public record struct ItemMaturingRequest(Quality quality, SellIn sellIn);
+}
