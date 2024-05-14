4089068df734b93116d2f7afe15f12ce3b25a2ce
Ksi <labani.said@outlook.com>
2024-05-15 00:12:23 +0200

    Add MaturingStrategy folder, AgedBrieMaturingStrategy.cs

---
+++ src/Core/ProposedCode/MaturingStrategies/AgedBrieMaturingStrategy.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AgedBrieMaturingStrategy.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.ProposedCode.MaturingStrategies
+{
+    internal class AgedBrieMaturingStrategy : IItemMaturingStrategy
+    {
+        public ItemMaturingResponse UpdateQuality(ItemMaturingRequest request)
+        {
+            var newQuality = request.quality;
+            if (newQuality.IsBelowLimit(50))
+            {
+                newQuality = newQuality.Increase();
+            }
+
+            var newSellIn = request.sellIn.Decrease();
+
+            if (newQuality.IsBelowLimit(50) && newSellIn.HasExpired())
+            {
+                newQuality = newQuality.Increase();
+            }
+
+            return new (newQuality, newSellIn);
+        }
+    }
+}
