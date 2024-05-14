2b2d10cb05d206df1caa54a226e3640a8fdafbf6
Ksi <labani.said@outlook.com>
2024-05-14 23:50:18 +0200

    Add Quality.cs

---
+++ src/Core/ProposedCode/Domain/Quality.cs
@@ -0,0 +1,10 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Quality.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.ProposedCode
+{
+    public record struct Quality(int value);
+}
