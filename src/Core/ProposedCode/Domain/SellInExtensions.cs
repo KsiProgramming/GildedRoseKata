321317572d7ce29e1f8a3ca56e142ed8812acbc5
Ksi <labani.said@outlook.com>
2024-05-14 23:55:08 +0200

    Add SellInExtensions.cs

---
+++ src/Core/ProposedCode/Domain/SellInExtensions.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SellInExtensions.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.ProposedCode
+{
+    public static class SellInExtensions
+    {
+        public static SellIn Decrease(this SellIn sellIn)
+        {
+            return new SellIn(sellIn.value - 1);
+        }
+
+        public static bool HasExpired(this SellIn sellIn)
+        {
+            return sellIn.value <= 0;
+        }
+
+        public static bool IsBelowLimit(this SellIn sellIn, int limit)
+        {
+            return sellIn.value < limit;
+        }
+    }
+}
