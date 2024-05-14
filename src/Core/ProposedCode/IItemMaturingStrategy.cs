69062291e44f8a2e78a61eacbe0211e9b323567a
Ksi <labani.said@outlook.com>
2024-05-15 00:06:15 +0200

    Add IItemMaturingStrategy.cs

---
+++ src/Core/ProposedCode/IItemMaturingStrategy.cs
@@ -0,0 +1,13 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IItemMaturingStrategy.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.ProposedCode
+{
+    public interface IItemMaturingStrategy
+    {
+        ItemMaturingResponse UpdateQuality(ItemMaturingRequest request);
+    }
+}
