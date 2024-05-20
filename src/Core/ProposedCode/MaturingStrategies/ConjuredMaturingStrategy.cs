963595564981f3b6b8ec95f0b4e1d7e3f488986b
Ksi <labani.said@outlook.com>
2024-05-20 22:58:20 +0200

    Add ConjuredMaturingStrategy.cs

---
+++ src/Core/ProposedCode/MaturingStrategies/ConjuredMaturingStrategy.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ConjuredMaturingStrategy.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.ProposedCode.MaturingStrategies
+{
+    public class ConjuredMaturingStrategy : IItemMaturingStrategy
+    {
+        public ItemMaturingResponse Update(ItemMaturingRequest request)
+        {
+            var newQuality = request.quality;
+            if (newQuality.IsAboveMinimum(0))
+            {
+                newQuality = newQuality.DecreaseBy(2);
+            }
+
+            var newSellIn = request.sellIn.Decrease();
+
+            if (newSellIn.HasExpired() && newQuality.IsAboveMinimum(0))
+            {
+                newQuality = newQuality.DecreaseBy(2);
+            }
+
+            return new (newQuality, newSellIn);
+        }
+    }
+}
