c54bf2f5d831f394f093a1d7d0a4ef6b1d0dab29
Ksi <labani.said@outlook.com>
2024-05-20 23:57:37 +0200

    Add IsBelowLimit test to QualityExtensionsTests.cs

--- tests/Core.Tests/ProposedCode/Domain/QualityExtensionsTests.cs
+++ tests/Core.Tests/ProposedCode/Domain/QualityExtensionsTests.cs
@@ -6,7 +6,21 @@
 
 namespace GildedRoseKata.ProposedCode.Tests
 {
+    using FluentAssertions;
+
     public class QualityExtensionsTests
     {
+        [Theory]
+        [InlineData(20, 50, true)]
+        [InlineData(50, 50, false)]
+        [InlineData(60, 50, false)]
+        public void IsBelowLimit(int qualityValue, int limit, bool expectedResult)
+        {
+            var quality = new Quality(qualityValue);
+
+            var result = quality.IsBelowLimit(limit);
+
+            result.Should().Be(expectedResult);
+        }
     }
 }
