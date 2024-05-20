cb59d3a8211bd09c61be6379b466b4550e9fcd34
Ksi <labani.said@outlook.com>
2024-05-21 00:16:13 +0200

    Add Decrease test to SellInExtensionsTests.cs

--- tests/Core.Tests/ProposedCode/Domain/SellInExtensionsTests.cs
+++ tests/Core.Tests/ProposedCode/Domain/SellInExtensionsTests.cs
@@ -6,7 +6,20 @@
 
 namespace GildedRoseKata.ProposedCode.Tests
 {
+    using FluentAssertions;
+
     public class SellInExtensionsTests
     {
+        [Theory]
+        [InlineData(5, 4)]
+        public void Decrease(int qualityValue, int expectedResult)
+        {
+            var quality = new Quality(qualityValue);
+
+            var result = quality.Decrease();
+
+            result.value.Should().Be(expectedResult);
+            result.Should().NotBeSameAs(quality);
+        }
     }
 }
