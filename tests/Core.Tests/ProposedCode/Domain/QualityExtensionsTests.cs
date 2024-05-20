d61009ea22698d263b0d2f6eb07d4a8c70b75664
Ksi <labani.said@outlook.com>
2024-05-20 23:50:44 +0200

    Add QualityExtensionsTests.cs

---
+++ tests/Core.Tests/ProposedCode/Domain/QualityExtensionsTests.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="QualityExtensionsTests.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.ProposedCode.Tests
+{
+    public class QualityExtensionsTests
+    {
+    }
+}
