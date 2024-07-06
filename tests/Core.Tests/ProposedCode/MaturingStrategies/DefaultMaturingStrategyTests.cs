310505d1f2ce4ca48de759d6700b1225b99b3dd8
Ksi <labani.said@outlook.com>
2024-07-07 01:00:39 +0200

    Add tests for Update of DefaultMaturingStrategy.cs in DefaultMaturingStrategyTests.cs

---
+++ tests/Core.Tests/ProposedCode/MaturingStrategies/DefaultMaturingStrategyTests.cs
@@ -0,0 +1,63 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DefaultMaturingStrategyTests.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GildedRoseKata.ProposedCode.MaturingStrategies.Tests;
+
+using FluentAssertions;
+
+public class DefaultMaturingStrategyTests
+{
+    [Fact]
+    public void Update()
+    {
+        var request = new ItemMaturingRequest
+        {
+            quality = new (0),
+            sellIn = new (2),
+        };
+
+        var stretagy = new DefaultMaturingStrategy();
+
+        var result = stretagy.Update(request);
+
+        result.quality.value.Should().Be(0);
+        result.sellIn.value.Should().Be(1);
+    }
+
+    [Fact]
+    public void Update_WithQualityIsAboveMinimum()
+    {
+        var request = new ItemMaturingRequest
+        {
+            quality = new (1),
+            sellIn = new (2),
+        };
+
+        var stretagy = new DefaultMaturingStrategy();
+
+        var result = stretagy.Update(request);
+
+        result.quality.value.Should().Be(0);
+        result.sellIn.value.Should().Be(1);
+    }
+
+    [Fact]
+    public void Update_WithQualityIsAboveMinimumAndSellInIsExpired()
+    {
+        var request = new ItemMaturingRequest
+        {
+            quality = new (1),
+            sellIn = new (1),
+        };
+
+        var stretagy = new DefaultMaturingStrategy();
+
+        var result = stretagy.Update(request);
+
+        result.quality.value.Should().Be(0);
+        result.sellIn.value.Should().Be(0);
+    }
+}
